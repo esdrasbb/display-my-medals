f2aac7ab486cf5f1fd8fd311ff013f99c7daaf37
Esdras Barreto <esdras.barreto@gmail.com>
2015-11-11 22:23:28 -0300

    created lein project

---
+++ test/display_my_medals/core_test.clj
@@ -0,0 +1,7 @@
+(ns display-my-medals.core-test
+  (:require [clojure.test :refer :all]
+            [display-my-medals.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
